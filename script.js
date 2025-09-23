5df799c1d04f0adb3d3a7275e8c5467bdd4084c4
khwongjeremy-netizen <kh.wong.jeremy@gmail.com>
2025-09-22 17:25:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var pag = document.getElementById("pages");
 
 function showMenu() {
+  console.log("Yo the menu showed my boy");
   pages.style.right = "0";
 }
 function hideMenu() {
+  console.log("Yo the menu  hid my boy");
   pages.style.right = "-200px";
 }
